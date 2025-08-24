562ccfbf71648992f7de0bcb6de7d0ec68ae96b3
Retr0 <megaoleg5884@gmail.com>
2025-08-24 23:12:14 +0300

    Add Task 2

--- src/Main.java
+++ src/Main.java
@@ -17,5 +17,21 @@ public class Main {
         System.out.println("Значение переменной memory с типом long равно: " + memory);
         System.out.println("Значение переменной pi с типом float равно: " + pi);
         System.out.println("Значение переменной a с типом double равно: " + a);
+
+        //Задача 2
+        System.out.println("\nЗадача 2");
+        float value1 = 27.12f;
+        long value2 = 987678965549L;
+        float value3 = 2.786f;
+        short value4 = 569;
+        int value5 = 27897;
+        byte value6 = 67;
+
+        System.out.println("value1" + value1);
+        System.out.println("value2" + value2);
+        System.out.println("value3" + value3);
+        System.out.println("value4" + value4);
+        System.out.println("value5" + value5);
+        System.out.println("value6" + value6);
     }
 }
