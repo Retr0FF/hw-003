2bbd6e27bae8a931f21a704c611b68230eaec635
Retr0 <megaoleg5884@gmail.com>
2025-08-24 22:58:10 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args)
+    {
+        System.out.println("Задача 1");
+        int myAge = 19;
+        byte numberOfStudents = 22;
+        short roomNumber = 405;
+        long memory = 123456789L;
+        float pi = 3.14f;
+        double a = 9.80665;
+
+        System.out.println("Значение переменной myAge с типом int равно: " + myAge);
+        System.out.println("Значение переменной numberOfStudents с типом byte равно: " + numberOfStudents);
+        System.out.println("Значение переменной roomNumber с типом short равно: " + roomNumber);
+        System.out.println("Значение переменной memory с типом long равно: " + memory);
+        System.out.println("Значение переменной pi с типом float равно: " + pi);
+        System.out.println("Значение переменной a с типом double равно: " + a);
+    }
+}
